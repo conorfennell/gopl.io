842630750d49ff0676d956f0a519cf1e3f936959
Conor Fennell <fennell.conor@gmail.com>
2019-04-07 17:02:18 +0100

    exercise one point four

--- ch1/dup2/main.go
+++ ch1/dup2/main.go
@@ -16,9 +16,10 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	fileCounts := make(map[string][]string)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, fileCounts)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -26,21 +27,25 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, fileCounts)
 			f.Close()
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
+			for _, fileName := range fileCounts[line] {
+				fmt.Printf("%s", fileName)
+			}
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, fileCounts map[string][]string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+		fileCounts[input.Text()] = append(fileCounts[input.Text()], f.Name())
 	}
 	// NOTE: ignoring potential errors from input.Err()
 }
