9709a591bdcd67d50b3e105e652c8f8ba0acac4f
Conor Fennell <fennell.conor@gmail.com>
2019-04-07 16:09:40 +0100

    one point two

--- ch1/echo3/main.go
+++ ch1/echo3/main.go
@@ -9,12 +9,13 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 //!+
 func main() {
-	fmt.Println(strings.Join(os.Args[0:], " "))
+	for index, argument := range os.Args[1:] {
+		fmt.Println(index, argument)
+	}
 }
 
 //!-
